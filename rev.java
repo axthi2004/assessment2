5d9a71fda13e725941a6e5acb0b035201a6bd841
axthi2004 <160604520+axthi2004@users.noreply.github.com>
2025-03-04 15:16:59 +0530

    rev.java

--- rev.java
+++ rev.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class rev {
     public static void main(String[] args) {
-        //Scanner scanner = new Scanner(System.in);
-       // String input = scanner.nextLine();
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
        String input= "I Love  Java Progrmming";
         String[] words = input.trim().split("\\s+");
         StringBuilder reversedSentence = new StringBuilder();
